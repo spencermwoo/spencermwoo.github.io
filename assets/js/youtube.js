589261d55d3ebb49c0ecf7be5c35eb91469bbb90
spencermwoo <smwoo2016@gmail.com>
2017-12-23 00:26:03 -0800

    change playlists

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -5,38 +5,37 @@ var numMusic = 0;
 var numHobby = 5; //starting place, id
 var TOTAL_MUSIC = numHobby - 1;
 
-//love, angst, bl1
 var playlists = [
     "PL4otHF15gsL0WQR1zewP18b1EjzgIJM2w", //welcome
-    "PL4otHF15gsL0-8_yKPgyxGdK4pKxIJ5Dw", //poprocks
+    "PL4otHF15gsL257xtn6iqZlRo55tMxgo4h", //1%
+    "PL4otHF15gsL0-8_yKPgyxGdK4pKxIJ5Dw", //milk
     "PL4otHF15gsL3ufeRvYZZj6mhA1J6e5Yk-", //meanies
-    "", //vroom vroom
-    "PL4otHF15gsL0NMtxBhm7_a0Lr2GHlBK38", //covers
 
     "PL4otHF15gsL1w2BS08GszHXikZXGBoPSS", //bhop
-    "", //comedy
+    "PL4otHF15gsL3kcZl44uv35z-PyWTK0EYS", //cardistry
     "PL4otHF15gsL3w2l_EddcwoEio4BJ9zqA-", //dance
-    "" //sermons
+    "PL4otHF15gsL1W0h_goKLrS8ICRzDUSPqH" //talks
 ];
+//poprocks, covers
+
 var names = [
     "welcome",
-    "poprocks",
+    "1%",
+    "milk",
     "meanies",
-    "vroom_vroom",
-    "covers",
 
     "bhop",
-    "comedy",
+    "cardistry",
     "dance",
-    "sermons"
+    "talks"
 ];
 
 $(document).ready(function() {
-    $('#3, #6, #8').click(function(){
-        alert('Apologies, coming soon.');
-    })
+    // $('#3, #6, #8').click(function(){
+    //     alert('Apologies, coming soon.');
+    // })
 
-    $('#0, #1, #2, #4, #5, #7').click(function() {
+    $('#0, #1, #2, #3, #4, #5, #6, #7').click(function() {
         var newNum = this.id;
         var num = numHobby;
 
